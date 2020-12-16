bd4aad61e00ad3fba7738d923d036b6e3b4de07c
Hayoun Lee <sidhlee@gmail.com>
2020-12-15 23:30:57 -0500

    fix: remove horizontal scrollbar on layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -13,7 +13,8 @@ import BackToTopButton from './back-to-top-button'
 export const StyledLayout = styled.div`
   position: relative;
   margin: 0 auto;
-  width: 100vw;
+  /* width: 100vw; */ // Don't use 100vw inside 100% container
+  width: 100%;
   height: 100%;
   min-height: 100vh;
   display: grid;
