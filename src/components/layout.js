14f0701628bcacdfc0f555d8bbb59a71fedfba48
Hayoun Lee <sidhlee@gmail.com>
2021-01-18 18:06:29 -0500

    fix: add meta tags with Helmet component

--- src/components/layout.js
+++ src/components/layout.js
@@ -52,6 +52,9 @@ const Layout = ({ children }) => {
   return (
     <>
       <Helmet>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Nayoun Kim, Illustrator</title>
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600;700&display=swap"
---
+++ src/utils/writeWorkMds.js
@@ -0,0 +1,7 @@
+const fs = require('fs')
+const path = require('path')
+
+function writeWorkMds() {
+  const works = fs.readdirSync('./assets/images/works')
+  works.forEach(work => {})
+}
