7398931b875e5b8e71657be40494fa9153151e7e
Hayoun Lee <sidhlee@gmail.com>
2021-01-27 21:33:43 -0500

    fix: now user can navigate images with tab

--- src/components/work.js
+++ src/components/work.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import styled from 'styled-components'
 import { Link } from 'gatsby'
 import PropTypes, { shape } from 'prop-types'
@@ -8,35 +8,48 @@ import { useLayoutDispatch, actionTypes } from '../store'
 const StyledWork = styled.div`
   position: relative;
   overflow: hidden;
-  .overlay-text {
-    position: absolute;
-    z-index: 10;
-    bottom: -100%;
-    color: var(--text-inverse);
-    padding: 0.5em 0.75em;
-    transition: bottom 200ms ease-in-out;
-  }
-  .overlay-bg {
-    position: absolute;
-    width: 101%;
-    height: 101%;
-    z-index: 7;
-    background-color: #000000;
-    opacity: 0;
-    transition: all 200ms ease-in-out;
-  }
-
-  &:hover {
+  a {
+    display: block;
+    width: 100%;
+    height: 100%;
     .overlay-text {
-      bottom: 0%;
+      position: absolute;
+      z-index: 10;
+      bottom: -100%;
+      color: var(--text-inverse);
+      padding: 0.5em 0.75em;
+      transition: bottom 200ms ease-in-out;
     }
     .overlay-bg {
-      opacity: 0.6;
+      position: absolute;
+      width: 101%;
+      height: 101%;
+      z-index: 7;
+      background-color: #000000;
+      opacity: 0;
+      transition: all 200ms ease-in-out;
+    }
+
+    &:hover,
+    &:focus {
+      .overlay-text {
+        bottom: 0%;
+      }
+      .overlay-bg {
+        opacity: 0.6;
+      }
     }
   }
 `
 
 const Work = ({ work, openOverlay, updateCurrentIndex }) => {
+  const ref = useRef()
+
+  // fix iphone6 chrome focusing the first work component on page load
+  useEffect(() => {
+    ref.current.blur()
+  }, [])
+
   // when changing image name, things can get weird due to the caching.
   // try deleting .cache folder and rebuild
   if (!work.node.frontmatter.featuredImage) {
@@ -52,7 +65,7 @@ const Work = ({ work, openOverlay, updateCurrentIndex }) => {
   }
 
   return (
-    <StyledWork className="image" id={work.node.frontmatter.slug}>
+    <StyledWork className="image" id={work.node.frontmatter.slug} ref={ref}>
       <a href={`#${work.node.frontmatter.slug}`} onClick={handleClick}>
         <div className="overlay-text">
           <figcaption>{work.node.frontmatter.title}</figcaption>
