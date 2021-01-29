24397f13a2c1e07248ac3e864e1514ba489bbd22
Hayoun Lee <sidhlee@gmail.com>
2021-01-28 21:15:31 -0500

    fix: replace menu icon with svg
    was not displaying on old iOS

--- src/components/menu-button.js
+++ src/components/menu-button.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
+import { FiMenu } from 'react-icons/fi'
 
 import { mq } from '../styles'
 
@@ -9,26 +10,13 @@ const StyledMenuButton = styled('button')`
   left: 1em;
   top: 50%;
   transform: translateY(-50%);
-
-  --size: max(30px, min(7vw, 38px));
+  --size: max(40px, min(7vw, 50px));
   width: calc(var(--size) * 1.1);
   height: var(--size);
   padding: 5px;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-
-  span {
+  svg {
     width: 100%;
-    height: 3px;
-    background: var(--text-main);
-    border-radius: 3px;
-  }
-
-  @media (min-width: 475px) {
-    /* position: relative;
-    margin-right: 0; // rel-pos child is contained in parent's content-box
-    bottom: 5px; */
+    height: 100%;
   }
 
   @media (min-width: ${mq.desktop}px) {
@@ -39,9 +27,7 @@ const StyledMenuButton = styled('button')`
 const MenuButton = ({ isOpen, handleClick }) => {
   return (
     <StyledMenuButton isOpen={isOpen} onClick={handleClick}>
-      <span className="top"></span>
-      <span className="middle"></span>
-      <span className="bottom"></span>
+      <FiMenu />
     </StyledMenuButton>
   )
 }
